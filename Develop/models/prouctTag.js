60410feee8b0b035cd3751df50002f2cbfbb38ed
Aisha Kimpson <akimp313@gmail.com>
2022-06-23 20:35:48 -0400

    updated models productTag.js

--- Develop/models/prouctTag.js
+++ Develop/models/prouctTag.js
@@ -6,8 +6,34 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      // Use the special Sequelize DataTypes object to provide the type of data.
+      type: DataTypes.INTEGER,
+      // This is equivalent to SQL's `NOT NULL` option.
+      allowNull: false,
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      // Use the special Sequelize DataTypes object to provide the type of data.
+      type: DataTypes.INTEGER,
+      // This is equivalent to SQL's `NOT NULL` option.
+      allowNull: false,
+      references: {
+        model: "tag",
+        key: "id",
+      },
+    },
   },
+
   {
     sequelize,
     timestamps: false,
