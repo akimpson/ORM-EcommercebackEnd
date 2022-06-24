08c602ed0f2326d5a56e979121a798e68c9cc772
Aisha Kimpson <akimp313@gmail.com>
2022-06-23 22:33:10 -0400

    updated routes api tag-routes.js

--- Develop/models/category.js
+++ Develop/models/category.js
@@ -7,17 +7,17 @@ class Category extends Model {}
 Category.init(
   {
     id: {
-      // use the special Sequelize DataTypes object to provide the type of data.
+      // Use the special Sequelize DataTypes object to provide the type of data.
       type: DataTypes.INTEGER,
       // Equivalent to SQL's `NOT NULL` option.
       allowNull: false,
       // show this is the Primary Key
       primaryKey: true,
-      // turn on auto increment
+      // Turn on auto increment.
       autoIncrement: true,
     },
     category_name: {
-      // use the special Sequelize DataTypes object to provide the type of data.
+      // Use the special Sequelize DataTypes object to provide the type of data.
       type: DataTypes.STRING,
       // Equivalent to SQL's `NOT NULL` option.
       allowNull: false,
