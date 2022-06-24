a556817773f7d978b3f8b58a4152a333cacba559
Aisha Kimpson <akimp313@gmail.com>
2022-06-23 20:32:09 -0400

    updated models product.js

--- Develop/models/product.js
+++ Develop/models/product.js
@@ -1,15 +1,48 @@
-// import important parts of sequelize library
+// Import the most important parts of the sequelize library.
 const { Model, DataTypes } = require("sequelize");
-// import our database connection from config.js
+// Import the database connection from config.js.
 const sequelize = require("../config/connection");
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+// Initialize Product model (table) by extending off Sequelize's Model class.
 class Product extends Model {}
 
-// set up fields and rules for Product model
+// Set up fields and rules for Product model.
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    product_name: {
+      // Use the special Sequelize DataTypes object to provide the type of data.
+      type: DataTypes.STRING,
+      // this is the equivalent of SQL's `NOT NULL` option
+      allowNull: false,
+    },
+    price: {
+      // Use the special Sequelize DataTypes object to provide the type of data.
+      type: DataTypes.INTEGER,
+      // this is the equivalent of SQL's `NOT NULL` option
+      allowNull: false,
+    },
+    stock: {
+      // Use the special Sequelize DataTypes object to provide the type of data.
+      type: DataTypes.INTEGER,
+      // This is equivalent to SQL's `NOT NULL` option.
+      allowNull: false,
+    },
+    category_id: {
+      // Use the special Sequelize DataTypes object to provide the type of data.
+      type: DataTypes.INTEGER,
+      // This is equivalent to SQL's `NOT NULL` option.
+      allowNull: true,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
